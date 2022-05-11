76410dd58db4bc95cbc00fd0c1840d65701c44e2
lingj1206 <lingj1206@masseyhigh.school.nz>
2022-05-12 09:55:28 +1200

    Update 04_text_bit_calculator.py

--- 04_text_bit_calculator.py
+++ 04_text_bit_calculator.py
@@ -3,8 +3,12 @@ def text_bits():
     print()
     var_text = input("Enter some text ")
     var_length = len(var_text)
-    num_nits = 8 * var_length
+    num_bits = 8 * var_length
 
     print()
     print("\'{}\' has {} characters ...".format(var_text, var_length))
-    
+    print("# of bits is {} x 8...".format(var_length))
+    print("We need {} bits to represent {}".format(num_bits, var_text))
+    print()
+
+    return ""
