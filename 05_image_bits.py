d508133a0fc413b260ba64c4dd414fd01da3ad61
lingj1206 <lingj1206@masseyhigh.school.nz>
2022-05-16 13:00:35 +1200

    Create 05_image_bits.py

---
+++ 05_image_bits.py
@@ -0,0 +1,29 @@
+
+def num_check(question, low):
+    valid = False
+    while not valid:
+
+        error = ("Please enter a number that is more than (or equal to) {} without a decimal point".format(low))
+
+        try:
+
+            response = int(input(question))
+            
+            if response >= low:
+                return response
+
+            else:
+                print(error)
+                print()
+        
+        except ValueError:
+            print(error)
+
+def image_bits () :
+    image_width = num_check("Image width? ", 1)
+    image_height = num_check("Image height? ", 1)
+
+    num_pixels = image_width * image_height
+
+    num_bits = num_pixels * 24
+     
